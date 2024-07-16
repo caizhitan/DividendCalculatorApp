e421abdc46a8aab6a8311913190acc888ffab3fe
caizhitan <caizhi@kailabs.art>
2024-07-16 23:33:26 +0800

    feat: add dynamic calculation to Total Investments amt

---
+++ Dividend Calculator/Utilities/FileManagerHelper.swift
@@ -0,0 +1,50 @@
+//
+//  FileManagerHelper.swift
+//  Dividend Calculator
+//
+//  Created by Cai Zhi Tan on 16/7/24.
+//
+
+import Foundation
+
+class FileManagerHelper {
+    static let shared = FileManagerHelper()
+    private init() {}
+    
+    private let fileName = "cards.json"
+    
+    private var fileURL: URL? {
+        do {
+            return try FileManager.default
+                .url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+                .appendingPathComponent(fileName)
+        } catch {
+            print("Error getting file URL: \(error.localizedDescription)")
+            return nil
+        }
+    }
+    
+    func saveCards(_ cards: [Card]) {
+        guard let fileURL = fileURL else { return }
+        
+        do {
+            let data = try JSONEncoder().encode(cards)
+            try data.write(to: fileURL)
+        } catch {
+            print("Error saving cards: \(error.localizedDescription)")
+        }
+    }
+    
+    func loadCards() -> [Card] {
+        guard let fileURL = fileURL else { return [] }
+        
+        do {
+            let data = try Data(contentsOf: fileURL)
+            let cards = try JSONDecoder().decode([Card].self, from: data)
+            return cards
+        } catch {
+            print("Error loading cards: \(error.localizedDescription)")
+            return []
+        }
+    }
+}
