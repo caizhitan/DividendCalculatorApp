7bd2cdb8b18f025e7abc87303c5d19124cc04dd0
caizhitan <caizhi@kailabs.art>
2024-07-16 18:23:01 +0800

    Removed Comments

--- Dividend Calculator/Components/CustomButton.swift
+++ Dividend Calculator/Components/CustomButton.swift
@@ -7,17 +7,17 @@ struct CustomButton: View {
                 .frame(width: 12, height: 2)
                 .foregroundColor(Color(red: 1, green: 1, blue: 1))
                 .cornerRadius(4)
-                .offset(y: 0) // Center the horizontal bar
+                .offset(y: 0)
             Rectangle()
                 .frame(width: 12, height: 2)
                 .foregroundColor(Color(red: 1, green: 1, blue: 1))
                 .cornerRadius(4)
                 .rotationEffect(.degrees(90))
-                .offset(x: 0) // Center the vertical bar
+                .offset(x: 0)
         }
         .frame(width: 24, height: 24)
         .background(Color(red: 0, green: 0.48, blue: 1))
-        .cornerRadius(12) // Adjust to half the width/height for a rounded button
+        .cornerRadius(12)
     }
 }
 
