c66a1d30f7afac135a70d97e632fba8ee62111c1
caizhitan <caizhi@kailabs.art>
2024-07-16 20:03:50 +0800

    Added: Passing of props for components

---
+++ Dividend Calculator/Components/MiniCardView.swift
@@ -0,0 +1,18 @@
+//
+//  MiniCardView.swift
+//  Dividend Calculator
+//
+//  Created by Cai Zhi Tan on 16/7/24.
+//
+
+import SwiftUI
+
+struct MiniCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MiniCardView()
+}
