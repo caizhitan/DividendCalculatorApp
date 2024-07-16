c0f8af905afbf7ae861a9d97102829c98b34aa29
caizhitan <caizhi@kailabs.art>
2024-07-16 23:01:01 +0800

    fix: Saving Stock Ticker Cards data. added: README.md

---
+++ Dividend Calculator/Components/AddCardView.swift
@@ -0,0 +1,18 @@
+//
+//  AddCardView.swift
+//  Dividend Calculator
+//
+//  Created by Cai Zhi Tan on 16/7/24.
+//
+
+import SwiftUI
+
+struct AddCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddCardView()
+}
---
+++ Dividend Calculator/Models/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  Dividend Calculator
+//
+//  Created by Cai Zhi Tan on 16/7/24.
+//
+
+import Foundation
