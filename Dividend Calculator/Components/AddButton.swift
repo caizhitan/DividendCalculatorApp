a62f42862e37f45e16708c1b4ce174634448e840
caizhitan <caizhi@kailabs.art>
2024-07-17 00:02:19 +0800

    feat: dynamic calculation of yearly income

---
+++ Dividend Calculator/Components/AddButton.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct CustomButton: View {
+    var action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            ZStack {
+                Rectangle()
+                    .frame(width: 12, height: 2)
+                    .foregroundColor(Color.white)
+                    .cornerRadius(4)
+                    .offset(y: 0)
+                Rectangle()
+                    .frame(width: 12, height: 2)
+                    .foregroundColor(Color.white)
+                    .cornerRadius(4)
+                    .rotationEffect(.degrees(90))
+                    .offset(x: 0)
+            }
+            .frame(width: 24, height: 24)
+            .background(Color.blue)
+            .cornerRadius(12)
+        }
+    }
+}
+
+struct CustomButton_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomButton(action: {})
+    }
+}
